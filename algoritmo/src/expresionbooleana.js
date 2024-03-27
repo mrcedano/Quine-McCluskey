a84f43fa53a9bfe9e21e2d4e93c9ce996b3b7e6b
mrcedano <oscarcedanob@gmail.com>
2024-03-27 12:56:23 -0700

    Separación de términos usando función recursiva.

--- algoritmo/src/expresionbooleana.js
+++ algoritmo/src/expresionbooleana.js
@@ -1,4 +1,45 @@
 "use strict";
-function obtenerExpresiones() {
-    return [""];
+function obtenerFragmentos(termino) {
+    var parentesis, corchete, llave;
+    var suma;
+    if (termino instanceof Array) {
+        for (var i = 0; i < termino.length; i++) {
+            var aux = obtenerFragmentos(termino[i]);
+            if (Array.isArray(aux)) {
+                return aux;
+            }
+            if (typeof aux == "string") {
+                aux = obtenerFragmentos(aux);
+                return aux;
+            }
+        }
+        return termino;
+    }
+    parentesis = termino.includes("(");
+    corchete = termino.includes("[");
+    llave = termino.includes("{");
+    if (!parentesis && !corchete && !llave) {
+        termino = termino.split("+");
+        return termino;
+    }
+    return [termino];
+}
+function aplicarValores(termino, valVariables) {
+    var auxTermino = termino;
+    obtenerFragmentos([termino]);
+    return 0;
+}
+function resolverExpresionBooleana(termino, valVariaables) {
+    var auxTermino = termino;
+    var arreglo = [];
+    arreglo.push(auxTermino);
+    while (typeof auxTermino != "number") {
+        var aux = obtenerFragmentos(arreglo);
+        console.log(aux);
+        break;
+    }
+    return 0;
+}
+resolverExpresionBooleana("AB+C+B+A+DFD", [1, 0, 1]);
+function obtenerResultado() {
 }
--- algoritmo/ts/expresionbooleana.ts
+++ algoritmo/ts/expresionbooleana.ts
@@ -3,7 +3,74 @@
  * Esta función recibe una expresión booleana y la fragmenta en términos independientes.
  * @returns string[]
  */
+function obtenerFragmentos(termino:string[]|string):string[]|string {   
+    let parentesis:boolean, corchete:boolean, llave:boolean;
+    let suma:number;
 
-function obtenerExpresiones():string[] {   
-    return [""];
+    if(termino instanceof Array) {
+        for (let i = 0; i < termino.length; i++) {
+            let aux:unknown = obtenerFragmentos(termino[i]);
+                if(Array.isArray(aux)){ 
+                    console.log(aux, "array");
+                    
+                }
+
+                if(typeof aux == "string"){
+                    aux = obtenerFragmentos(aux);
+                    console.log(aux);
+                       
+                }
+        }
+        return termino;
+    }
+    parentesis = termino.includes("(");
+    corchete = termino.includes("[");
+    llave = termino.includes("{");
+
+    if(!parentesis && !corchete && !llave){
+        termino = termino.split("+");
+        return termino;
+    }
+    
+    return [termino];
+
+}
+/**
+ * Esta función recibirá un término de una expresión y los valores de cada término. 
+ * Se aplica la jerarquia de operaciones.
+ * @param termino
+ * @param valVariables Los valores de las variables que se aplicarán al término.
+ * @returns El resultado del t"érmino según los valores de sus variables.
+ */
+function aplicarValores(termino:string, valVariables:number[]):number {
+    let auxTermino:string[]|string|number = termino;
+
+    obtenerFragmentos([termino]);
+    return 0;
+}
+
+function resolverExpresionBooleana(termino:string,valVariaables:number[]):number {
+    let auxTermino:string|number = termino;
+    let arreglo:string[] = [];
+    arreglo.push(auxTermino);
+
+    while(typeof auxTermino != "number") {
+        let aux = obtenerFragmentos(arreglo);
+        // Haciendo la suma...
+
+        console.log(aux);
+        
+        break;
+    }
+    
+
+    return 0;
+}
+
+//resolverExpresionBooleana("AB[B(C+A)+C[A+C]]", [1,0,1]);
+resolverExpresionBooleana("AB+C", [1,0,1])
+
+
+function obtenerResultado() {
+
 }
