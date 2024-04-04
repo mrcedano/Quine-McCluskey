5547b280ab4d2bdeac00f938605c1dc57c8bf263
Ch1v1st4 <164919732+Ch1v1st4@users.noreply.github.com>
2024-04-04 11:30:23 -0700

    miniterminos.ts

--- algoritmo/ts/miniterminos.ts
+++ algoritmo/ts/miniterminos.ts
@@ -1,4 +1,29 @@
 function obtenerMiniterminos(){
-    
+type Term = string;
+type PrimeImplicant = {
+    term: Term;
+    used: boolean;
+};
+
+function findPrimeImplicants(expression: Term[]): PrimeImplicant[] {
+    const primeImplicants: PrimeImplicant[] = [];
+    for (const term of expression) {
+        primeImplicants.push({ term: term, used: false });
+    }
+    return primeImplicants;
+}
+
+function simplifyExpression(expression: Term[]): Term[] {
+    const primeImplicants = findPrimeImplicants(expression);
+    return expression;
+}
+    function simplifyQuineMcCluskey(inputExpression: string): string {
+    const expression: Term[] = inputExpression.split('|');
+    const simplifiedExpression = simplifyExpression(expression);
+    return simplifiedExpression.join('|');
+}
+    const userInput = "(A&B)|(C&D)";
+const simplifiedExpression = simplifyQuineMcCluskey(userInput);
+console.log("Expresión simplificada:", simplifiedExpression);
 }
     
