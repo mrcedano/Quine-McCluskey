650485862cb12b953f2fdc287e15ba73a923a095
mrcedano <oscarcedanob@gmail.com>
2024-04-04 21:40:15 -0700

    Aplicando método de Quine-McCluskey
    Aún falta terminar el algoritmo...

--- algoritmo/src/tablaverdad.js
+++ algoritmo/src/tablaverdad.js
@@ -1,18 +1,26 @@
 "use strict";
-function obtenerTablaVerdad(numeroEntradas) {
-    if (numeroEntradas <= 1) {
-        throw new RangeError("Los números de entrada deben de ser mayor a 1");
-    }
-    var entradas = Math.pow(numeroEntradas, 2);
-    var tablaverdad = [];
-    var format = "0".repeat(numeroEntradas);
-    for (var count = 0; count < entradas; count++) {
-        var representacion = conversionDecimalaBinario(count);
-        var resultado = format.concat(representacion).substring(representacion.length);
-        console.log(resultado);
-    }
-    return tablaverdad;
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+var expresionbooleana_1 = require("./expresionbooleana");
+var miniterminos_1 = require("./miniterminos");
 function conversionDecimalaBinario(decimal) {
     return (decimal >>> 0).toString(2);
 }
+function obtenerTablaVerdad(expresion, numeroEntradas) {
+    if (numeroEntradas <= 1)
+        throw new RangeError("Los números de entrada deben de ser mayor a 1");
+    var CASOS = Math.pow(2, numeroEntradas);
+    var FORMATO = "0".repeat(numeroEntradas);
+    var NUEVATABLA = [];
+    for (var count = 0; count < CASOS; count++) {
+        var BINARIO = conversionDecimalaBinario(count);
+        var RESULTADO_1 = FORMATO.concat(BINARIO).substring(BINARIO.length).split("");
+        NUEVATABLA.push(RESULTADO_1.map(function (val) { return parseInt(val); }));
+    }
+    for (var count = 0; count < CASOS; count++) {
+        NUEVATABLA[count][numeroEntradas] = (0, expresionbooleana_1.default)(expresion, NUEVATABLA[count]);
+    }
+    return NUEVATABLA;
+}
+var RESULTADO = obtenerTablaVerdad("A+B", 2);
+var PRIMOS_IMPLICANTES = (0, miniterminos_1.obtenerPrimosImplicantes)(RESULTADO);
+var AGRUPACION_PRIMOS_IMPLICANTES = (0, miniterminos_1.agruparPrimosImplicantes)(PRIMOS_IMPLICANTES);
--- algoritmo/ts/tablaverdad.ts
+++ algoritmo/ts/tablaverdad.ts
@@ -1,27 +1,5 @@
-/**
- * Devuelve una tabla de verdad en un arreglo de X-dimensiones según 
- * la cantidad de entradas que se especifique.
- * @returns number[]
- */
-function obtenerTablaVerdad(numeroEntradas:number, ):number[][] {
-    if(numeroEntradas <= 1) {
-        throw new RangeError("Los números de entrada deben de ser mayor a 1")
-    }
-
-    const entradas:number = Math.pow(numeroEntradas, 2)
-    const tablaverdad:number[][] = []
-
-    const format:string = "0".repeat(numeroEntradas)
-    
-    for(let count = 0; count < entradas; count++) {
-       let representacion:string = conversionDecimalaBinario(count)
-       let resultado:string = format.concat(representacion).substring(representacion.length)
-        console.log(resultado);
-    }
-    
-    return tablaverdad
-}
-
+import resolverExpresionBooleana from "./expresionbooleana";
+import { obtenerPrimosImplicantes, agruparPrimosImplicantes } from "./miniterminos";
 /**
  * @param decimal Número Decimal
  * @returns Representación binaria del número decimal.
@@ -29,6 +7,32 @@ function obtenerTablaVerdad(numeroEntradas:number, ):number[][] {
 function conversionDecimalaBinario(decimal:number):string {
     return (decimal >>> 0).toString(2);
 }
+/**
+ * Devuelve una tabla de verdad en un arreglo de X-dimensiones según 
+ * la cantidad de entradas que se especifique.
+ * @returns Arreglo de X-dimensiones.
+ */
+function obtenerTablaVerdad(expresion:string,numeroEntradas:number):number[][] {
+    if(numeroEntradas <= 1) throw new RangeError("Los números de entrada deben de ser mayor a 1")
+    
+    const CASOS:number = Math.pow(2, numeroEntradas)
+    const FORMATO:string = "0".repeat(numeroEntradas)
+    const NUEVATABLA:number[][] = []
+    
+    for(let count = 0; count < CASOS; count++) {
+       const BINARIO:string = conversionDecimalaBinario(count)
+       const RESULTADO:string[] = FORMATO.concat(BINARIO).substring(BINARIO.length).split("")        
+       NUEVATABLA.push(RESULTADO.map((val) => parseInt(val))) 
+    }
+
+    for(let count = 0; count < CASOS; count++) {
+        NUEVATABLA[count][numeroEntradas] = resolverExpresionBooleana(expresion, NUEVATABLA[count])
+    }
+    
+    return NUEVATABLA;
+}
+const RESULTADO:number[][] = obtenerTablaVerdad("A+B",2)
+const PRIMOS_IMPLICANTES:number[][] = obtenerPrimosImplicantes(RESULTADO)
+const AGRUPACION_PRIMOS_IMPLICANTES = agruparPrimosImplicantes(PRIMOS_IMPLICANTES)
 
 
- 
